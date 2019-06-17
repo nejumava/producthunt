2d7c74a5f9d1fdef53746fcc77382d164d2d1f5c
Nelly Mahecha <nejumava@gmail.com>
2019-06-17 12:05:21 -0500

    Implementa la funcionalidad de autenticarse

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -40,8 +40,29 @@ class ProductsController < ApplicationController
         redirect_to products_path, notice: "El producto fue eliminado con éxito"
     end
 
+    def sign_in(user)
+        cookies.permanent.signed[:user_id] = user.id
+        @current_user = user
+    end
+
+    def sign_out
+        cookies.delete(:user_id)
+        @current_user = nil
+    end
+
     private
         def product_params
             params.require(:product).permit(:name, :url, :description)
         end
+
+        def signed_in?
+            !current_user.nil?
+        end
+        helper_method :signed_in?
+        
+        def current_user
+            @current_user ||= User.find(cookies.signed[:user_id]) if cookies.signed[:user_id]
+            rescue ActiveRecord::RecordNotFound
+        end
+        helper_method :current_user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #get '/products', to: 'products#index'
-  #get '/products/new', to: 'products#new', as: 'new_product'
-  #post '/products', to: 'products#create'
-  #get '/products/:id', to: 'products#show', as: 'product'
-  #get '/products/:id/edit', to: 'products#edit', as: 'edit_product'
-  #patch '/products/:id', to: 'products#update'
-  #delete '/products/:id', to: 'products#destroy'
-  resources :products
   root 'products#index'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+
+  resources :users, only: [:new, :create]
+  resources :products
 end
